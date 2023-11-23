98db8b4b1cc94cc5900b992308e31feb00220b09
Aribabo <wjdgms1004@naver.com>
2023-11-23 15:28:28 +0900

    리액트 기초자료 업데이트

---
+++ Client/src/App.jsx
@@ -0,0 +1,53 @@
+import { Switch, Route, useHistory } from 'react-router-dom';
+import Header from './components/Header';
+import AllTweets from './pages/AllTweets';
+import MyTweets from './pages/MyTweets';
+import { useAuth } from './context/AuthContext';
+
+// mvc 패턴처럼 화면과 로직을 분리
+// jsx는 화면을 그리는 쪽
+
+function App({ tweetService }) {
+  const history = useHistory();
+  const { user, logout } = useAuth();
+
+  const onAllTweets = () => {
+    history.push('/');
+  };
+
+  const onMyTweets = () => {
+    history.push(`/${user.username}`);
+  };
+
+  const onLogout = () => {
+    if (window.confirm('Do you want to log out?')) {
+      logout();
+      history.push('/');
+    }
+  };
+
+  return (
+    <div className='app'>
+      <Header
+        username={user.username}
+        onLogout={onLogout}
+        onAllTweets={onAllTweets}
+        onMyTweets={onMyTweets}
+      />
+      <Switch>
+        (
+        <>
+          <Route exact path='/'>
+            <AllTweets tweetService={tweetService} />
+          </Route>
+          <Route exact path='/:username'>
+            <MyTweets tweetService={tweetService} />
+          </Route>
+        </>
+        )
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
